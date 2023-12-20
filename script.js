3c7a0899912e77915e55311b394eaa7438b36b7b
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-12-20 22:39:19 +0530

    update

--- script.js
+++ script.js
@@ -1,86 +1,47 @@
-var rest_api = "https://restcountries.com/v3.1/all";
+const data=fetch("https://restcountries.com/v3.1/all").then((response)=>response.json()).then((result)=>printData(result)).catch((err)=>console.log(err));
+const container=document.getElementById('container');
+const arr=[];
+function printData(result){
+    result.map((d)=>{
+        // console.log(d.latlng);
+    //     const rowDiv=document.createElement('div');
+    //     rowDiv.setAttribute("class","row col-lg-4 col-sm-12 me-2")
+    //    const cardDiv= document.createElement('div');
+    //    cardDiv.setAttribute("class","card p-2 my-2")
+    //     const headerDiv=document.createElement('div');
+    //     headerDiv.setAttribute("class","card-header text-center bg-black text-white");
+    //     headerDiv.innerHTML=d.name.common;
+    //     cardDiv.appendChild(headerDiv);
+    //     rowDiv.appendChild(cardDiv);
 
-async function api() {
-  var url = fetch(rest_api);
-  var out = await (await url).json();
-  console.log(out[0]);
-  var parent = document.querySelector(".row");
-  for (let i of out) {
-    try {
-      console.log(i);
-      var data_cont = document.createElement("div");
-      data_cont.classList.add("card");
+        const value=`<div class="row col-lg-4 col-sm-12">
+        <div class="card">
+          <div class="card-header text-center bg-black text-white">
+            ${d.name.common}
+          </div>
+          <div class="card-body text-center ">
+            <img src=${d.flags.png} width="80%" height="70%"/>
+            <div class="capital text-center text-white pt-3">
+              Captial: ${d.capital}
+            </div>
+            <div class="region text-center text-white">
+                Region:${d.region}
+            </div>
+            <div class="code text-center text-white">
+                Country Code:${d.cca3}
+            </div>
+          </div>
+          <a  href="https://api.open-meteo.com/v1/forecast?latitude=${d.latlng[0]}&longitude=${d.latlng[1]}&current_weather=true" class="btn btn-primary">
+          Click for Weather</a>
+          
+        </div>
+    </div>`
+    arr.push(value);
+    
 
-      //latlng
-      var lat = i.latlng[0];
-      var lng = i.latlng[1];
-      data_cont.setAttribute("lat", lat);
-      data_cont.setAttribute("lng", lng);
-
-      //Name
-      var country_name = document.createElement("h3");
-      country_name.innerText = i.name.common;
-      data_cont.append(country_name);
-
-      //Flag
-      var country_flag = document.createElement("img");
-      country_flag.setAttribute("src", i.flags.png);
-      data_cont.append(country_flag);
-
-      //Capital
-      var country_capital = document.createElement("p");
-      country_capital.innerText = "Capital : " + i.capital[0];
-      data_cont.append(country_capital);
-       console.log(i.capital[0]);
-
-      //Region
-      var country_region = document.createElement("p");
-      country_region.innerText = "Region : " + i.region;
-      data_cont.append(country_region);
-
-      //Country codes
-      var country_code = document.createElement("p");
-      country_code.innerText = "Contry Code : " + i.cca3;
-      data_cont.append(country_code);
-
-      var country_population = document.createElement("p");
-      country_population.innerText = "population : " + i.population
-      data_cont.append(country_population);
-
-      //Click Button
-      var click_btn = document.createElement("button");
-      click_btn.setAttribute("onclick", "clicking(this)");
-      click_btn.innerHTML = "Click for Weather";
-      data_cont.append(click_btn);
-      console.log(click_btn);
-
-      parent.append(data_cont);
-    } catch (err) {
-    console.log(err);
     }
-  }
-}
-api();
+    );
 
-async function clicking(e) {
-  var parent = e.parentElement;
-console.log(e.parentElement);
-  var lat = parent.getAttribute("lat");
-  var lon = parent.getAttribute("lng");
-  var api_key = "02c0a4cb6d534fb8ec504bd1d2f69fe2";
-  var weather_api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
-  var weather_url = fetch(weather_api);
-  var out1 = await (await weather_url).json();
-  console.log(out1);
-  var result = document.querySelector("body div.container");
-  result.innerHTML = "";
-  var data_ele = document.createElement("h1");
-  data_ele.classList.add("cName");
-  var data_ele1 = document.createElement("p");
-  data_ele.classList.add("cWeather");
-   console.log(data_ele.innerText);
-  data_ele.innerText = JSON.stringify(out1.sys.country);
-  data_ele1.innerText = JSON.stringify(out1.weather);
-  result.append(data_ele);
-  result.append(data_ele1);
+    container.innerHTML=arr;
 }
+
